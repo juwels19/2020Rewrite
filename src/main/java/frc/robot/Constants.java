a6aec662c82660a69b3f68aeb3a880d7ba4194a3
Julian Curalli <julian.curalli27@gmail.com>
2020-03-09 23:17:15 -0400

    Initial Commit

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -9,7 +9,39 @@ package frc.robot;
 
 public class Constants {
 
-    
+    // Controllers
+    public static final int DRIVER_CONTROLLER_PORT = 0;
+    public static final int OPERATOR_CONTROLLER_PORT = 1;
+    public static final double DRIVER_CONTROLLER_DEADBAND = 0.2;
+    public static final double OPERATOR_CONTROLLER_DEADBAND = 0.2;
 
+    // Drive
+    public static final int DRIVE_LEFT_MASTER_CAN_ID = 0;
+    public static final int DRIVE_LEFT_SLAVE_CAN_ID = 1;
+    public static final int DRIVE_RIGHT_MASTER_CAN_ID = 2;
+    public static final int DRIVE_RIGHT_SLAVE_CAN_ID = 3;
+
+    // Intake/Elevator
+    public static final int SIDE_ROLLER_INTAKE_CAN_ID = 5;
+    public static final int ELEVATOR_MOTOR_CAN_ID = 6;
+    public static final int DINGUS_MOTOR_CAN_ID = 10;
+
+    // Shooter
+    public static final int LEFT_SHOOTER_CAN_ID = 7;
+    public static final int RIGHT_SHOOTER_CAN_ID = 8;
+
+    // Pneumatics
+    public static final int INTAKE_CYLINDERS_FORWARD_PORT = 0;
+    public static final int INTAKE_CYLINDERS_REVERSE_PORT = 1;
+    public static final int TENSIONER_CYLINDERS_FORWARD_PORT = 2;
+    public static final int TENSIONER_CYLINDERS_REVERSE_PORT = 3;
+
+    // LEDs
+    public static final int LED_PWM_PORT = 0;
+    public static final int LED_COUNT = 40;
+
+    // Limelight
+    public static final double LIMELIGHT_YAW_THRESHOLD = 1.0;
+    public static final double LIMELIGHT_THROTTLE_THRESHOLD = 1.0;
 
 }
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,11 +7,60 @@
 
 package frc.robot;
 
+import com.kauailabs.navx.frc.AHRS;
+import com.revrobotics.CANEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.AddressableLED;
+import edu.wpi.first.wpilibj.AddressableLEDBuffer;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.SerialPort;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+  // Controllers
+  private XboxController driverController = new XboxController(Constants.DRIVER_CONTROLLER_PORT);
+  private XboxController operatorController = new XboxController(Constants.OPERATOR_CONTROLLER_PORT);
+
+  // Drivetrain 
+  private CANSparkMax leftMaster = new CANSparkMax(Constants.DRIVE_LEFT_MASTER_CAN_ID, MotorType.kBrushless);
+  private CANSparkMax leftSlave = new CANSparkMax(Constants.DRIVE_LEFT_SLAVE_CAN_ID, MotorType.kBrushless);
+  private CANSparkMax rightMaster = new CANSparkMax(Constants.DRIVE_RIGHT_MASTER_CAN_ID, MotorType.kBrushless);
+  private CANSparkMax rightSlave = new CANSparkMax(Constants.DRIVE_RIGHT_SLAVE_CAN_ID, MotorType.kBrushless);
+  private CANEncoder leftEnc = leftMaster.getEncoder();
+  private CANEncoder rightEnc = rightMaster.getEncoder();
+  private AHRS navx = new AHRS(SerialPort.Port.kMXP);
+  private DifferentialDriveOdometry odometry;
+  private boolean isReversed = false;
+  private double maxOutput = 1.0;
+
+  // Intake
+
+  // Shooter
+
+  // Pneumatics
+  private DoubleSolenoid intakeCyls = new DoubleSolenoid(Constants.INTAKE_CYLINDERS_FORWARD_PORT, Constants.INTAKE_CYLINDERS_REVERSE_PORT);
+  private DoubleSolenoid tensionerCyls = new DoubleSolenoid(Constants.TENSIONER_CYLINDERS_FORWARD_PORT, Constants.TENSIONER_CYLINDERS_REVERSE_PORT);
+
+  // Vision
+  private NetworkTable limelight = NetworkTableInstance.getDefault().getTable("limelight");
+  private double tx, ty, tv;
+
+  // LEDs
+  private AddressableLED led = new AddressableLED(Constants.LED_PWM_PORT);
+  private AddressableLEDBuffer ledBuffer = new AddressableLEDBuffer(Constants.LED_COUNT);
+  private String ledState = "disabled";
+
+  // Auto
   private static final String kDefaultAuto = "Default";
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
@@ -19,6 +68,10 @@ public class Robot extends TimedRobot {
 
   @Override
   public void robotInit() {
+    driveInit();
+    visionInit();
+
+
     m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
     m_chooser.addOption("My Auto", kCustomAuto);
     SmartDashboard.putData("Auto choices", m_chooser);
@@ -37,12 +90,29 @@ public class Robot extends TimedRobot {
   }
 
   @Override
+  public void disabledInit() {
+    DriverStation.reportWarning("In disabledInit", false);
+  }
+
+  @Override
+  public void disabledPeriodic() {
+    DriverStation.reportWarning("In disabledPeriodic", false);
+    if ((isXAligned() && isYAligned()) && ledState.equals("disabled")) {
+      ledAligned();
+      ledState = "aligned";
+    } else if ((isXAligned() || isYAligned()) && ledState.equals("aligned")) {
+      ledDisabled();
+      ledState = "disabled";
+    }
+  }
+
+  @Override
   public void autonomousInit() {
     m_autoSelected = m_chooser.getSelected();
     // m_autoSelected = SmartDashboard.getString("Auto Selector", kDefaultAuto);
     System.out.println("Auto selected: " + m_autoSelected);
+    ledAutoRunning();
   }
-
 
   @Override
   public void autonomousPeriodic() {
@@ -58,10 +128,175 @@ public class Robot extends TimedRobot {
   }
 
   @Override
+  public void teleopInit() {
+    DriverStation.reportWarning("In teleopInit", false);
+    ledTeleop();
+  }
+
+  @Override
   public void teleopPeriodic() {
   }
 
   @Override
   public void testPeriodic() {
   }
+
+
+
+
+
+
+
+
+
+
+  // Drivetrain Methods
+  public void driveInit() {
+    leftMaster.restoreFactoryDefaults();
+    leftSlave.restoreFactoryDefaults();
+    rightMaster.restoreFactoryDefaults();
+    rightSlave.restoreFactoryDefaults();
+
+    rightMaster.setInverted(true);
+    rightSlave.setInverted(true);
+    leftMaster.setInverted(false);
+    leftSlave.setInverted(false);
+
+    rightSlave.follow(rightMaster);
+    leftSlave.follow(leftMaster);
+
+    leftEnc.setPositionConversionFactor(0.4); 
+    rightEnc.setPositionConversionFactor(0.4);
+
+    resetDriveEncoders();
+    resetGyro();
+    odometry = new DifferentialDriveOdometry(Rotation2d.fromDegrees(getHeading()));
+  }
+
+  public void drive(double throttle, double yaw) {
+    if (isReversed()) {
+      leftMaster.set(maxOutput * (throttle+yaw));
+      rightMaster.set(maxOutput * (throttle-yaw));
+
+    } else {
+      leftMaster.set(maxOutput * (-throttle+yaw)); //-throttle+yaw
+      rightMaster.set(maxOutput * (-throttle-yaw)); //-throttle-yaw
+    }
+    
+  }
+
+  public boolean isReversed() {
+    return isReversed;
+  }
+
+  public void toggleReversed() {
+    isReversed = !isReversed;
+  }
+
+  public void resetDriveEncoders() {
+    leftEnc.setPosition(0.0);
+    rightEnc.setPosition(0.0);
+  }
+
+  public void resetGyro() {
+    navx.zeroYaw();
+  }
+
+  public double getHeading() {
+    return Math.IEEEremainder(navx.getAngle(), 360);
+  }
+
+
+
+
+
+
+
+
+
+
+
+  // Vision Methods
+  public void visionInit() {
+    setLimelightLED(1.0);
+    setLimelightCamMode(0.0);
+  }
+
+  public void updateLimelightValues() {
+    tx = limelight.getEntry("tx").getDouble(0.0);
+    ty = limelight.getEntry("ty").getDouble(0.0);
+    tv = limelight.getEntry("tv").getDouble(0.0);
+  }
+
+  public void setLimelightLED(double state) {
+    // 1 = force off, 2 = force blink, 3 = force on
+    limelight.getEntry("ledMode").setNumber(state);
+  }
+
+  public void setLimelightCamMode(double mode) {
+    // 0 = vision processor, 1 = Driver Camera
+    limelight.getEntry("camMode").setNumber(mode);
+  }
+
+  public boolean isXAligned() {
+    return Math.abs(tx) < Constants.LIMELIGHT_YAW_THRESHOLD;
+  }
+
+  public boolean isYAligned() {
+    return Math.abs(ty) < Constants.LIMELIGHT_THROTTLE_THRESHOLD;
+  }
+
+  public boolean hasTarget() {
+    return tv == 1.0;
+  }
+
+
+
+
+
+
+
+
+  // LED methods
+  public void ledInit() {
+    led.setLength(ledBuffer.getLength());
+    led.setData(ledBuffer);
+    led.start();
+    ledDisabled();
+  }
+
+  public void ledDisabled() {
+    for (int i = 0; i < ledBuffer.getLength(); i = i + 2) { // pattern: blue and gold alternating
+      ledBuffer.setRGB(i, 212, 175, 55);
+      ledBuffer.setRGB(i+1, 0, 0, 102);
+    }
+    led.setData(ledBuffer);
+  }
+
+  public void ledAutoRunning() {
+    for (int i = 0; i < ledBuffer.getLength(); i++) { // pattern: solid blue
+      ledBuffer.setRGB(i, 0, 0, 255);
+    }
+    led.setData(ledBuffer);
+  }
+
+  public void ledTeleop() {
+    for (int i = 0; i < ledBuffer.getLength(); i++) { // pattern: white
+      ledBuffer.setRGB(i, 255, 255, 255);
+    }
+    led.setData(ledBuffer);
+  }
+
+  public void ledAligned() {
+    for (int i = 0; i < ledBuffer.getLength(); i++) { // pattern: solid green
+      ledBuffer.setRGB(i, 0, 255, 0);
+    }
+    led.setData(ledBuffer);
+  }
+
+
+
+
+
+
 }
